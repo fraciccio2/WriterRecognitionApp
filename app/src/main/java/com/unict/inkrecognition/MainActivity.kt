b1c1736fa5370db94e35b57632fa5c1df650d35f
giacomazzof <francesco.giacomazzo@rbtech.dev>
2024-10-07 16:52:43 +0200

    feat: first commit

---
+++ app/src/main/java/com/unict/inkrecognition/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.unict.inkrecognition
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+    }
+}
